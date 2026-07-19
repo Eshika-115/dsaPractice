ce07c3952e931a73f4041e30c14dd04e7cad81cd
Eshika_01 <eshikamathur01@gmail.com>
2026-07-20 00:03:50 +0530

    Time: 0 ms (100%), Space: 49.5 MB (93.37%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool canReach(vector<int>& start, vector<int>& target) {
-        return (start[0] + start[1])%2 == (target[0] + target[1])%2;
+        if( (start[0] + start[1])%2 == (target[0] + target[1])%2){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
