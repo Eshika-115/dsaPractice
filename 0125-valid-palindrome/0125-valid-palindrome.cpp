403c771f449221bb22e668d8923b4e79182ec455
Eshika_01 <eshikamathur01@gmail.com>
2026-07-26 00:30:38 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
 
-bool Is_Alpha_num(char ch){
+//bool Is_Alpha_num(char ch){
 
-    if((ch>= '0' && ch<= '9') || (tolower(ch)>= 'a' && tolower(ch)<='z' )){
-        return true;
-    }
-    return false;
+   // if((ch>= '0' && ch<= '9') || (tolower(ch)>= 'a' && tolower(ch)<='z' )){
+      //  return true;
+   // }
+    //return false;
 
-}
+//}
+
+
     bool isPalindrome(string s) {
         int i =0;
         int j =s.length()-1;
-
         while(i<j){
-            if(!Is_Alpha_num(s[i])){
-                i++;
-                continue;
-            }
-            if(!Is_Alpha_num(s[j])){
-                j--;
-                continue;
-        }
+
+        while(i<j && !isalnum(s[i])){ i++;}
+        while(i<j && !isalnum(s[j])){ j--;}
 
         if(tolower(s[i])!=tolower(s[j])){
             return false;
