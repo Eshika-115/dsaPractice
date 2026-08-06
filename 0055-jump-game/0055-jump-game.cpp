80fe438d4ee706a4d0c9256d5e95756a9fb26078
Eshika_01 <eshikamathur01@gmail.com>
2026-08-06 16:30:21 +0530

    Time: 0 ms (100%), Space: 52.5 MB (25.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int max_reach = 0; // Hum maximum kahan tak pahunch sakte hain
-        
-        for (int i = 0; i < n; i++) {
-            // Agar current index tak hum pahunch hi nahi sakte
-            if (i > max_reach) {
+        int n =nums.size();
+        int maxreach=0;
+        for(int i =0;i<n;i++){
+
+            if(i>maxreach)
                 return false;
-            }
-            
-            // Har step par max reach ko update karo 
-            max_reach = max(max_reach, i + nums[i]);
-            
-            //  Agar max reach last index tak ya usse aage chali gayi
-            if (max_reach >= n - 1) {
-                return true;
-            }
+
+            maxreach= max(maxreach, i + nums[i]);
+            if(maxreach>=n-1)
+            return true;
         }
-        
-        return false; 
-    }
+        return true;
+         }
 };
