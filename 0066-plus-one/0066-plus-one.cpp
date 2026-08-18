8347786485029ce9fad505423eb9382ba16d01d5
Eshika_01 <eshikamathur01@gmail.com>
2026-08-19 02:46:11 +0530

    Time: 0 ms (100%), Space: 11.4 MB (86.51%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+
+       // case 2,4,5
         for(int i =digits.size()-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]++;
@@ -9,7 +11,12 @@ public:
 
         digits[i]=0;
         }
-        digits.insert(digits.begin(),1);
-        return digits;
+        // case [9,9,9]
+        vector<int> ans(digits.size()+1,0);
+        ans[0]=1;
+        return ans;
+        
+       // digits.insert(digits.begin(),1);
+        //return digits;
     }
 };
